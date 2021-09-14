3edbec9e7e4f087d5819cbbe66f22c53b0bd15ce
ashikahmedfahim <41462787+ashikahmedfahim@users.noreply.github.com>
2021-09-14 22:14:12 +0600

    server init and db connection init

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const port = process.env.PORT || 8000;
+const MongoDB = process.env.DB;
+
+mongoose
+  .connect(MongoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((error) => console.log(error));
+
+app.listen(port, () => {
+  console.log(`Server is running at ${port}`);
+});
