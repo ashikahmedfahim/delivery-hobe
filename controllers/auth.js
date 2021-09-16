44116adf0350c7e41afd433ec2380436b37c6cbf
ashikahmedfahim <41462787+ashikahmedfahim@users.noreply.github.com>
2021-09-16 20:15:38 +0600

    user and admin auth added and middlewares implemented

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,54 @@
 const Admin = require("../models/admins");
 const User = require("../models/users");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const validators = require("../utilities/dataValidators");
+const ExpressError = require("../utilities/expressError");
 
-module.exports.adminLogin = (req, res) => {};
-module.exports.userLogin = (req, res) => {};
+module.exports.adminLogin = async (req, res) => {
+  const isValidData = validators.isValidUserObject(req.body);
+  if (isValidData.error) throw new ExpressError(400, isValidData.error.message);
+  const isAdmin = await Admin.findOne({ email: req.body.email });
+  if (isAdmin) {
+    const isValidAdmin = await bcrypt.compare(
+      req.body.password,
+      isAdmin.password
+    );
+    if (isValidAdmin) {
+      const token = jwt.sign(
+        { _id: isAdmin._id, email: isAdmin.email },
+        process.env.SECRETKEY
+      );
+      res.status(200).header("x-auth-token", token).send({ token });
+    } else {
+      res.status(400).send({ message: "Invalid credentials" });
+    }
+  } else {
+    res.status(404).send({ message: "Invalid credentials" });
+  }
+};
+module.exports.userLogin = async (req, res) => {
+  const isValidData = validators.isValidUserObject(req.body);
+  if (isValidData.error) throw new ExpressError(400, isValidData.error.message);
+  const isUser = await User.findOne({ email: req.body.email });
+  if (isUser) {
+    const isValidUser = await bcrypt.compare(
+      req.body.password,
+      isUser.password
+    );
+    if (isValidUser) {
+      const token = jwt.sign(
+        {
+          _id: isUser._id,
+          email: isUser.email,
+        },
+        process.env.SECRETKEY
+      );
+      res.status(200).send({ token });
+    } else {
+      res.status(400).send({ message: "Invalid credentials" });
+    }
+  } else {
+    res.status(404).send({ message: "Invalid credentials" });
+  }
+};
---
+++ middlewares/index.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const Admin = require("../models/admins");
+const ExpressError = require("../utilities/expressError");
+
+module.exports.isAuthenticated = (req, res, next) => {
+  try {
+    const token = req.headers["x-auth-token"];
+    if (!token) throw new ExpressError(400, "Token not found");
+    jwt.verify(token, process.env.SECRETKEY, (error, decoded) => {
+      if (error) throw new ExpressError(400, "Invalid Token");
+      console.log(decoded);
+      req.credentials = decoded;
+    });
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.isAuthorizedAsAdmin = async (req, res, next) => {
+  try {
+    const isAdmin = await Admin.findOne({ _id: req.credentials._id });
+    if (isAdmin.length) next();
+    throw new ExpressError(400, "Not Authorized");
+  } catch (error) {
+    next(error);
+  }
+};
